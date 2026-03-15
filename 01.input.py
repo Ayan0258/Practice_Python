fa0250517fc44f33a5a086c645aa087fc58183e4
Ayan <ayandasanu666666@gmail.com>
2026-03-15 15:27:18 +0530

    add python practice files

---
+++ 01.input.py
@@ -0,0 +1,17 @@
+# a = int(input("Enter the Number 1 "))
+# b = int(input("Enter the Number 2 "))
+
+# print("Number a is: ", a)
+# print("Number b is: ", b)
+# print("Sum is ", a + b)
+
+# a = input("the value of the a: ")
+# print(type(a))
+
+
+def poop():
+    print("Good day")
+
+
+poop()
+poop()
---
+++ forfun.py
@@ -0,0 +1,36 @@
+# n = int(input("Enter your number: "))
+
+# for i in range(1, n+1):
+
+#     if i == 1 or i == 2 or i == n:
+#         print("*"*i)
+
+#     else:
+#         print("*", end="")
+#         print(" "*(i-2), end="")
+#         print("*")
+
+"""
+
+*****
+*   *
+*   *
+*   *
+*
+
+"""
+
+n=int(input("Enter the number: "))
+
+for i in range(1, n+1):
+
+    if(i==1):
+        print("*"*n)
+    elif(i==n):
+        print("*")
+
+    else:
+        print("*",end="")
+        print(" "*(n-2),end="")
+        print("*")
+    print("")
