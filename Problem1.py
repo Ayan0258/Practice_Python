af2bba1da0c75ebca32cdfb019b4650c74019223
Ayan <ayandasanu666666@gmail.com>
2026-03-16 22:33:11 +0530

    today test

--- Problem1.py
+++ Problem1.py
@@ -339,4 +339,45 @@
 
 # print("github test")
 
-lets see if this ubdate or not
+# lets see if this ubdate or not
+
+
+# def greatest(a, b, c):
+#     if (a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c
+    
+# a = 1
+# b = 6
+# c = 3
+
+# print(greatest(a, b, c))
+
+# def f_to_c(f):
+#     return 5*(f-32)/9
+
+# f =int(input("Enter the teamparature in F: "))
+# e = f_to_c(f)
+# print(f"{round(e,3)}c")
+
+
+
+# def star(n):
+#     if (n==0):
+#         return
+#     print("*" * n)
+#     star(n-1)
+
+# star(6)
+
+def rem(l,word):
+    n=[]
+    for item in l:
+        if not(item == word):
+            n.append(item.strip(word))  
+    return n    
+l = ["Harry", "Ayan", "Anu", "Anushka", "an"]
+print(rem(l,"an"))
