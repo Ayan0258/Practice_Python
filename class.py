172024d2c8eeb02192f00e92976586b07b815010
Ayan Das <ayandasanu666666@gmail.com>
2026-03-26 00:07:29 +0530

    int self

--- class.py
+++ class.py
@@ -1,7 +1,51 @@
+# class Employe:
+#     role = "Ai Engineer"
+#     salary = 200000
+
+# Ayan = Employe()
+# Ayan.name = "AyanDas"
+# print(Ayan.name,Ayan.role,Ayan.salary)
+
+# Ram = Employe()
+# Ram.name = "Ram lal"
+# print(Ram.name,Ram.role,Ram.salary)
+
+# /////staticmethod//
+
+# class Employe:
+#     role = "Ai Engineer"
+#     salary = 200000
+
+
+#     def getinfo(self):
+#         print(f"The role is: {self.role},The salary ammount is: {self.salary}")
+
+#     @staticmethod
+#     def greet():
+#         print("Good morning")
+
+
+# Ayan = Employe()
+# Ayan.greet()
+# Ayan.getinfo() 
+
+# ///constractor.//
+
+
 class Employe:
     role = "Ai Engineer"
     salary = 200000
 
+    def __init__(self):
+        print("I am creating an object") #///dunder method which is automacticly called
+
+    def getinfo(self):
+        print(f"The role is: {self.role},The salary ammount is: {self.salary}")
+
+    @staticmethod
+    def greet():
+        print("Good morning")
+
+
 Ayan = Employe()
-Ayan.name = "Ayan Das"
 print(Ayan.role,Ayan.salary)
