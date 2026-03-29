752896c850c7afee4475b860aa435c5b055127e7
Ayan Das <ayandasanu666666@gmail.com>
2026-03-29 18:37:04 +0530

    class method

--- inheritance.py
+++ inheritance.py
@@ -1,23 +1,46 @@
-class Employe:
-    def __init__(self):
-        print("Constractor of Employe")
+"""
+how to use super
 
+"""
+
+
+
+# class Employe:
+#     def __init__(self):
+#         print("Constractor of Employe")
+
+#     a = 1
+
+
+
+# class Programer(Employe):
+#     def __init__(self):
+#         print("Constractor of prgramer")
+
+#     b = 2
+
+# class Manager(Programer):
+#     def __init__(self):
+#         super().__init__()
+#         print("Constractor of Manager")
+
+#     c = 3
+
+# o = Manager()
+# print(o.a, o.b, o.c)
+
+"""
+how to use class method
+"""
+
+class Employe:
     a = 1
 
+    @classmethod
+    def show(cls):
+        print(f"The class atribute of a is {cls.a}")
 
+e = Employe()
 
-class Programer(Employe):
-    def __init__(self):
-        print("Constractor of prgramer")
-
-    b = 2
-
-class Manager(Programer):
-    def __init__(self):
-        super().__init__()
-        print("Constractor of Manager")
-
-    c = 3
-
-o = Manager()
-print(o.a, o.b, o.c)
+e.a = 45
+e.show()
